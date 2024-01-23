118a6e6c178eaa48397ae26c2ae5839c80733f09
Jaime Kurzweg <99841509+TremblingInferno@users.noreply.github.com>
2024-01-23 14:39:25 -0700

    Edited regex to include commas within number

--- binarify.js
+++ binarify.js
@@ -1,6 +1,9 @@
 function switchToBinary(portion, match) {
 	let i = parseInt(match, 10);
 	let b = i.toString(2);
+	if (match[-1] == ','){
+		b.push(',');
+	}
 
 	return b;
 }
@@ -8,6 +11,6 @@ function switchToBinary(portion, match) {
 
 findAndReplaceDOMText(document.body, {
 	preset: 'prose',
-	find: /\d+/g,
+	find: /(?:\d{4,}|\d{1,3}(?:,\d{3})*)(?:\.\d+)?/gm,
 	replace: switchToBinary,
 });
