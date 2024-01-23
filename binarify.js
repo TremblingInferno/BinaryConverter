d0173cca2e8557121ba8e948de62c09b025339cd
Jaime Kurzweg <99841509+TremblingInferno@users.noreply.github.com>
2024-01-23 15:04:24 -0700

    Added support for superscript numbers

--- binarify.js
+++ binarify.js
@@ -1,16 +1,12 @@
 function switchToBinary(portion, match) {
 	let i = parseInt(match, 10);
 	let b = i.toString(2);
-	if (match[-1] == ','){
-		b.push(',');
-	}
-
 	return b;
 }
 
 
 findAndReplaceDOMText(document.body, {
 	preset: 'prose',
-	find: /(?:\d{4,}|\d{1,3}(?:,\d{3})*)(?:\.\d+)?/gm,
+	find: /(?:\d{4,}|\d{1,3}(?:,\d{3})*)(?:\.\d+)?/g,
 	replace: switchToBinary,
 });
