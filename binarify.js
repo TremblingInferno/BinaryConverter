a43e02c3fa4d37bd1d221f3d9326e074c306b67b
Jaime Kurzweg <99841509+TremblingInferno@users.noreply.github.com>
2024-01-24 15:56:16 -0700

    Added support for radix point

--- binarify.js
+++ binarify.js
@@ -1,7 +1,65 @@
+
+console.log(binaryFraction("0.091"));
+
 function switchToBinary(portion, match) {
-	let i = parseFloat(match, 10);
+	let f = "."+(match+"").split(".")[1];
+
+	let bf = binaryFraction(f);
+
+	let i = parseInt(match, 10);
+	
 	let b = i.toString(2);
-	return b;
+	return b + bf;
+}
+
+function binaryFraction(f){
+	const ERR = f/10;
+	if (!f){
+		return "";
+	}
+	let arr = [];
+	let seen = [];
+	let i = 0;
+	while (true){ 
+		if (i>28){
+			arr[i] = "...";
+			break;
+		}
+		if (!f){
+			break;
+		}
+		f *= 2;
+		let apprx_id = approx_in(f, seen, ERR);
+		if (apprx_id != -1) {
+			let new_arr = [...arr.slice(0, apprx_id), "r", ...arr.slice(apprx_id)];
+			arr = new_arr;
+			break;
+		}
+		if (!f){
+			break;
+		} else if (f >= 1){
+			arr[i] = 1;
+			f = "."+(f+"").split(".")[1];
+		} else {
+			arr[i] = 0;
+		}
+		seen[i] = f;
+		i++;
+	}
+	if (arr.length != 0){
+		return "." + arr.join("");
+	}
+	return arr.join("");
+}
+
+function approx_in(f, seen, ERR){
+	console.log(seen);
+	for(let i = 0; i < seen.length; i++){
+		if (Math.abs(f - seen[i]) < ERR) {
+			return i;
+		}
+	}
+	return -1;
 }
 
 
